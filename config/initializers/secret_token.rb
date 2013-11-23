91b74846905b82c70df4b18a52d0f5836a8dc075
Haruki <haruki@haruki-VirtualBox.(none)>
2013-11-23 11:43:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirehoseApp::Application.config.secret_key_base = 'dda7b5e606af83a985bd009f1069c86786f48f54ceedc97c61a0b73cf32a46162533cf1a40f347d27529a741e56f7f6b3c3a4aa1baf2ea758405f52b1c0bb12d'
